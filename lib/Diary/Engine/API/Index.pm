3ec3af8913a60f432bd89f5427c6cf869e211b42
skozawa <argentcrescent@gmail.com>
2012-03-12 20:30:51 +0900

    create add and delete function

--- lib/Diary/Engine/API/Index.pm
+++ lib/Diary/Engine/API/Index.pm
@@ -29,7 +29,78 @@ sub default : Public {
     }
 }
 
+## エントリの追加
+sub add : Public {
+    my ($self, $r) = @_;
+    
+    $r->follow_method;
+}
 
+sub _add_get {
+}
+
+sub _add_post {
+    my ($self, $r) = @_;
+    
+    $r->req->form(
+        title => ['NOT_BLANK'],
+        body => ['NOT_BLANK'],
+    );
+    
+    if (not $r->req->form->has_error) {
+        my $title = $r->req->param('title');
+        my $category = $r->req->param('category');
+        my $body = $r->req->param('body');
+
+        ## エントリの追加
+        my $entry = $r->user->add_entry (
+            title => $title,
+            body => $body,
+        );
+        ## カテゴリの追加
+        $r->user->add_category(
+            category => $category,
+            entry_id => $entry->id,
+        );
+
+        $r->stash->param(
+            entry => $entry,
+        );
+    }
+}
+
+
+## エントリの削除
+sub delete : Public {
+    my ($self, $r) = @_;
+    
+    my $id = $r->req->param('id');
+    my $entry = $id ? moco('Entry')->find( id => $id, user_id => $r->user->id ) : undef;
+    
+    ## エントリの確認
+    if ( !$entry ) {
+        $r->res->redirect('/');
+        return;
+    }
+    
+    print STDERR "+++\n";
+    
+    $r->follow_method;
+}
+
+sub _delete_post {
+    my ($self, $r) = @_;
+    
+    $r->req->form(
+        id => ['NOT_BLANK','UINT'],
+    );
+    
+    if (not $r->req->form->has_error) {
+        $r->user->delete_entry(entry_id => $r->req->param('id'));
+    }
+}
+
+## エントリの編集
 sub edit : Public {
     my ($self, $r) = @_;
     
--- static/js/jquery.editEntry.js
+++ static/js/jquery.editEntry.js
@@ -9,7 +9,14 @@
 	$.extend ( $.editEntry.prototype, {
 		/* HTMLをパースし、エントリのタイトル、カテゴリ、本文、作成日時を取得 */
 		getEntryItem : function () {
+			/* 新規作成の場合はHTMLをパースしない */
+			if ( this.id == "new_entry" ) {
+				this.title = "";
+				this.body = "";
+				return;
+			}
 			var $section = $("#entry_list").find("section#" + this.id);
+			
 			var $header = $section.find('header');
 			var $children = $header.children('a');
 			var category = {};
@@ -23,18 +30,31 @@
 			this.created_on = $section.find('footer').text();
 			
 		},
-		/* 編集フォームを開く */
+		
+		/* フォームを開く */
 		openForm : function () {
 			var edit = this;
 			var $section = $("#entry_list").find("section#" + this.id);
-			/* 編集用フォームの作成 */
+			/* フォームの作成 */
 			$section.html(this.createForm());
 			
 			var $buttons = $section.find('button');
-			/* 「閉じる」ボタンにイベントを割り当てる */
-			$($buttons[0]).click( function () {	edit.closeForm(); });
-			/* 「保存」ボタンにイベントを割り当てる */
-			$($buttons[1]).click( function () { edit.postEntry(); });
+			/* 新規作成の場合 */
+			if ( this.id == "new_entry" ) {
+				/* 「閉じる」ボタンにイベントを割り当てる */
+				$($buttons[0]).click( function () {	edit.closeAddForm(); });
+				/* 「保存」ボタンにイベントを割り当てる */
+				$($buttons[1]).click( function () { edit.postEntry(); });
+			}
+			/* 編集の場合 */
+			else {
+				/* 「閉じる」ボタンにイベントを割り当てる */
+				$($buttons[0]).click( function () {	edit.closeModifyForm(); });
+				/* 「保存」ボタンにイベントを割り当てる */
+				$($buttons[1]).click( function () { edit.postEntry(); });
+				/* 「削除」ボタンにイベントを割り当てる */
+				$($buttons[2]).click( function () { edit.deleteEntry(); });
+			}
 			
 			/* フォームに変更があった場合、データを一時的に保存する */
 			$section.find("input,textarea").change(function() { edit.tempStore(); });
@@ -48,7 +68,7 @@
 				$section.find("#title").before($div);
 			}
 		},
-		/* 編集用のフォームの作成 */
+		/* フォームの作成 */
 		createForm : function () {
 			/* 連想配列からカテゴリ名を取得し、配列に保存 */
 			var category_texts = [];
@@ -62,23 +82,38 @@
 			+ "<p>本文<textarea name='body' id='body'>" + this.body + "</textarea></p>"
 			+ "<p class='submit_button'>"
 			+ "<button type='button'>閉じる</button>"
-			+ "<button type='button'>保存</button>"
-			+ "</p>";
+			+ "<button type='button'>保存</button>";
+			if ( this.id != "new_entry" ) {
+				html += "<button type='button'>削除</button>";
+			}
+			html += "</p>";
 			
 			return html;
 		},
+		
+		/* 作成用フォームを閉じる */
+		closeAddForm : function () {
+			var edit = this;
+			var $section = $("#entry_list").find("section#new_entry");
+			/* 「作成」ボタン */
+			$section.html("<button type='button'>作成</button>");
+			
+			/* 「作成」ボタンにイベントを割り当てる */
+			$section.find('button').click(function () {	edit.openForm(); });
+		},
+		
 		/* 編集用フォームを閉じる */
-		closeForm : function () {
+		closeModifyForm : function () {
 			var edit = this;
 			var $section = $("#entry_list").find("section#" + this.id);
 			/* 表示用HTMLの作成 */
-			$section.html(this.createEntry());
+			$section.html(this.createDisplay());
 			
 			/* 「編集」ボタンにイベントを割り当てる */
 			$section.find('button').click(function () {	edit.openForm(); });
 		},
 		/* 表示用のHTMLを作成 */
-		createEntry : function () {
+		createDisplay : function () {
 			var html = ""
 			+ "<header><a href='/diary?id=" + this.id + "'>" + this.title + "</a>"
 			+ " [ ";
@@ -93,20 +128,28 @@
 			
 			return html;
 		},
-		/* 編集内容をAjaxで /API/index.editに送信 */
+		
+		/* エントリをPostで送信する */
 		postEntry : function () {
 			var edit = this;
 			var data = this.makePostData();
-			
-			$.ajax ({
-				url : "/API/index.edit",
-				type : "POST",
-				data : data,
-				datatype : "json",
-				success : function (res) { edit.successAjax(res); },
-				error : edit.errorAjax,
-				complete : function () {edit.completeAjax();},
-			});
+			var url = "/API/index.edit";
+			if ( this.id == "new_entry" ) {
+				url = "/API/index.add";
+			}
+			if ( data.title != "" && data.body != "" ) {
+				$.ajax ({
+					url : url,
+					type : "POST",
+					data : data,
+					datatype : "json",
+					success : function (res) { edit.successPostEntry(res); },
+					error : edit.errorAjax,
+					complete : function () { edit.completePostEntry();},
+				});
+			} else {
+				alert("タイトル及び本文は必須です");
+			}
 		},
 		/* Postするデータを作成 */
 		makePostData : function () {
@@ -115,40 +158,79 @@
 			var category = $section.find('#category').attr('value');
 			var body = $section.find('#body').attr('value');
 			
-			return {
-				id : this.id,
+			var data = {
 				title : title,
 				category : category,
 				body : body,
 			};
+			if ( this.id != "new_entry" ) {
+				data.id = this.id;
+			}
+			return data;
 		},
-		/* XHRに成功した場合、タイトルなどの値を更新し、編集用フォームを閉じる */
+		
+		/* XHRに成功した場合、タイトルなどの値を更新し、フォームを閉じる */
 		/* XHRが完了するまでは、「編集」ボタンにイベントを追加しない */
-		successAjax : function (res) {
-			//alert("success");
+		successPostEntry : function (res) {
 			var data = eval("(" + res + ")");
 			this.title = data.title;
 			this.categories = data.categories;
 			this.body = data.body;
-			
+						
 			var edit = this;
 			var $section = $("#entry_list").find("section#" + this.id);
-			$section.html(this.createEntry());
+			/* 新規作成の場合 */
+			if ( this.id == "new_entry" ) {
+				this.id = data.id;
+				this.created_on = data.created_on;
+				$section.attr("id", this.id);
+				/* 「作成」ボタンを追加 */
+				createAddButton();
+			}
+			$section.html(this.createDisplay());
 			
 			/* 一時データは削除 */
 			this.tempData = undefined;
 		},
-		/* XHRに失敗した場合、編集用フォームを閉じる */
-		errorAjax : function () {
-			alert("error");
-			this.closeForm();
-		},
 		/* XHRが完了したら、「編集」ボタンにイベントを追加 */
-		completeAjax : function () {
+		completePostEntry : function () {
 			var edit = this;
 			var $section = $("#entry_list").find("section#" + this.id);
 			$section.find('button').click(function () {	edit.openForm(); });
 		},
+		
+		
+//-----------------------------------------
+		
+		/* 削除するIDを送信 */
+		deleteEntry : function () {
+			var edit = this;
+			data = { id : this.id };
+			
+			if ( window.confirm("削除しますか？", "「" + this.title + "」の削除") ) {
+				$.ajax ({
+					url : "/API/index.delete",
+					type : "POST",
+					data : data,
+					datatype : "json",
+					success : function (res) { edit.successDelete(); },
+					error : edit.errorAjax,
+					complete : function () { edit.completeDelete(); }
+				});
+			}
+		},
+		/* エントリのノードを削除 */
+		successDelete : function () {
+			$("section#" + this.id).empty();
+		},
+		completeDelete : function () {
+			
+		},
+		/* XHRに失敗した場合、編集用フォームを閉じる */
+		errorAjax : function () {
+			alert("error");
+		},
+		
 		/* フォームに変更があった場合、データを一時的に保存しておく */
 		tempStore : function () {
 			var $section = $("#entry_list").find("section#" + this.id);
@@ -179,6 +261,21 @@
 
 })(jQuery);
 
+/* 「作成」ボタンの作成 */
+var createAddButton = function () {
+	var $section = $("<section/>");
+	$section.attr("id","new_entry");
+	var $button = $("<button/>");
+	$button.attr("type", "button");
+	$button.text("作成");
+	var $edit = this;
+	$button.click(function () {
+		new $.editEntry("new_entry").openForm();
+	});
+	$section.append($button);
+	$("#entry_list").prepend($section);
+}
+
 /* 「編集」ボタンを作成 */
 var createEditButton = function () {
 	$("section").each( function () {
@@ -197,5 +294,6 @@ var createEditButton = function () {
 }
 
 $(function() {
+	createAddButton();
 	createEditButton();
 });
